c99364003835c3609062d03871081147875f9637
Alessandro della Frattina <alessandrodellafrattina@gmail.com>
2022-10-19 09:30:16 +0200

    Added Application.java file

---
+++ Alilux/Convertions/Application.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        //Frame creation
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Set the frame size
+        frame.setSize(800, 500);
+        
+        //Show the frame
+        frame.setVisible(true);
+    }
+}
